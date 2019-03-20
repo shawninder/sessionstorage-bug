fb4b9aabe01e06ce31378e18b6e3cadde920d4dd
Shawn Inder <shawninder@gmail.com>
2019-03-20 19:14:02 -0400

    Map props to state with effect to fix hydration bug

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
+import { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 
 function App (props) {
+  const [showNotice, setShowNotice] = useState()
+  useEffect(() => {
+    setShowNotice(props.app.showNotice)
+  })
   return (
     <div>
-      <p>showNotice: {props.app.showNotice ? 'true' : 'false'}</p>
+      <p>showNotice: {showNotice ? 'true' : 'false'}</p>
       <button onClick={(event) => {
         props.dispatch({ type: 'App:toggleNotice' })
       }}>toggle</button>
       <div
         className='notice'
         style={{
-          opacity: props.app.showNotice ? 1 : 0,
-          transform: props.app.showNotice ? 'translateX(0)' : 'translateX(-100%)'
+          opacity: showNotice ? 1 : 0,
+          transform: showNotice ? 'translateX(0)' : 'translateX(-100%)'
         }}
       >
         Notice!
